e5d478b122aeb45f89a371992e5176a44b108d08
Sewer 56 <seweratwork@gmail.com>
2022-09-12 11:55:59 +0100

    Added: Cross-Platform Build Script

---
+++ Source/BuildToolsCrossplatform.ps1
@@ -0,0 +1,12 @@
+﻿# Set Working Directory
+Split-Path $MyInvocation.MyCommand.Path | Push-Location
+
+# Build
+dotnet publish ./RidersArchiveTool/RidersArchiveTool.sln -c Release --self-contained=false -o ./Build/CrossPlatform
+dotnet publish ./RidersTextureArchiveTool/RidersTextureArchiveTool.sln -c Release --self-contained=false -o ./Build/CrossPlatform
+dotnet publish ./IndexTool/IndexTool/IndexTool.sln -c Release --self-contained=false -o ./Build/CrossPlatform
+dotnet publish ./LayoutToJsonTool/LayoutToJson/LayoutToJson.sln -c Release --self-contained=false -o ./Build/CrossPlatform
+dotnet publish ./GcaxDatInjector/GcaxDatInjector.sln -c Release --self-contained=false -o ./Build/CrossPlatform
+
+# Restore Working Directory
+Pop-Location
